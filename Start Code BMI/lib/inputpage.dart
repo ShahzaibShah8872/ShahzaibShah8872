f3fc805497e28ae430199f05a11d93707ecba992
Shahzaib Shah <Shahzaibshah8872@gmail.com>
2022-04-14 05:53:37 +0500

    Refactor and code
    Refactor and Male female complete

--- Start Code BMI/lib/inputpage.dart
+++ Start Code BMI/lib/inputpage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -17,54 +19,82 @@ class _InputPageState extends State<InputPage> {
           Expanded(child: Row(
             children: [
               Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Colors.amberAccent,
-                    borderRadius: BorderRadius.circular(10.0),
+                child: RepeatContainerCode(
+                  colors: Colors.amberAccent,
+                  cardWidget: RepeatTextandIconWidget(
+                    iconData: FontAwesomeIcons.male,
+                    label: 'MALE',
                   ),
                 ),
               ),
-              Expanded(child:  Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Colors.amberAccent,
-                  borderRadius: BorderRadius.circular(10.0),
+              Expanded(child:  RepeatContainerCode(colors: Colors.amberAccent,
+                cardWidget: RepeatTextandIconWidget(
+                  iconData: FontAwesomeIcons.female,
+                  label: 'FEMALE',
                 ),
               ),
               ),
             ],
           ),),
-          Expanded(child:  Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Colors.amberAccent,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-          ),
+          Expanded(child:  RepeatContainerCode(colors: Colors.amberAccent,),
           ),
           Expanded(child: Row(
           children: [
-          Expanded(child:  Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Colors.amberAccent,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
+          Expanded(child:  RepeatContainerCode(colors: Colors.amberAccent,),
           ),
-          ),
-          Expanded(child:  Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Colors.amberAccent,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-          ),
+          Expanded(child:  RepeatContainerCode(colors: Colors.amberAccent,),
           ),
         ],
-      ),),
+      ),
+          ),
         ],
       )
+    );
+  }
+}
+
+class RepeatTextandIconWidget extends StatelessWidget {
+  RepeatTextandIconWidget({@ required this.iconData,this.label});
+  final IconData iconData;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          iconData,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(label,style: TextStyle(
+          fontSize: 18.0,
+          color: Colors.black,
+        ),
+        ),
+      ],
+
+    );
+  }
+}
+
+class RepeatContainerCode extends StatelessWidget {
+  RepeatContainerCode({@ required this.colors,this.cardWidget});
+  final Color colors;
+  final Widget cardWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      child: cardWidget,
+      decoration: BoxDecoration(
+        color:colors,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
     );
   }
 }
